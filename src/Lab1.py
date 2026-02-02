30eefe8fbe3574cd29d915df50cf7c5589df63b3
Ram Niwash <rniwash122@gmail.com>
2026-02-02 19:28:37 +0530

    Updated Lab1

--- src/Lab1.py
+++ src/Lab1.py
@@ -1,13 +1,19 @@
-# Print Simple message
+# Print Simple message Example of single line comment
 print("Hello World")
 
 print("Hello","World")
 
 print("Hello","World", sep='-')
 
+'''
+Example of single line comment
+Read the inputs from user as two number
+print on console
+add these numbers 
+print that numbers
+'''
 inputText = input("Please two numbers: ")
-print("Input =",inputText)
-
+print("inputText1 =",inputText)
 a,b = inputText.split()
 c = int(a)+int(b)
 print("A =",a)
