99a7da2549cae61a4cfec13fb8becedb755f8dc2
Ram Niwash <rniwash122@gmail.com>
2026-02-07 15:39:03 +0530

    Updated Lab1

--- src/Lab1.py
+++ src/Lab1.py
@@ -1,9 +1,15 @@
 # Print Simple message Example of single line comment
 print("Hello World")
 
-print("Hello","World")
+print("Hello", "World")
 
-print("Hello","World", sep='-')
+print("Hello", "World", sep='-')
+
+print("Hello", end="\t")
+
+# print special character
+print("Hello\nGuys")
+print("Python\tProgramming")
 
 '''
 Example of single line comment
@@ -12,10 +18,31 @@ print on console
 add these numbers 
 print that numbers
 '''
-inputText = input("Please two numbers: ")
-print("inputText1 =",inputText)
-a,b = inputText.split()
-c = int(a)+int(b)
-print("A =",a)
-print("B =",b)
-print("c =",c)
+# inputText = input("Please two numbers: ")
+# print("inputText1 =", inputText)
+# a, b = inputText.split()
+# c = int(a) + int(b)
+# print("A =", a)
+# print("B =", b)
+# print("c =", c)
+
+# Multi-variable assignment
+numa="10"
+numb="20"
+inuma, inumb = int(numa), int(numb)
+sum = inuma + inumb
+print(sum)
+
+
+multiLineText = '''
+This is a multi-line comment.
+It spans multiple lines.
+'''
+print(multiLineText)
+
+a = "Namaste"
+b = "Hello"
+c = "Namaskāram"
+
+sentence = f"Sanskrit = {a}; English = {b}; Malayalam = {c}"
+print(sentence)
